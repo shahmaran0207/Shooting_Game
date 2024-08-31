d6b9194c084f4a541a22f5f5e162dc04687fb8c8
shahmaran0207 <shahmaran0207@naver.com>
2024-08-31 13:48:59 +0900

    플레이어 border
    플레이어 border

---
+++ Shooting_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 curPos=transform.position;
+        Vector3 nextPos=new Vector3(h, v, 0)*speed*Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+}
