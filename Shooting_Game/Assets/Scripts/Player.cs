7b8c2fcc131e9aa9d017dd91ba3a1678939dab53
shahmaran0207 <shahmaran0207@naver.com>
2024-08-31 14:51:50 +0900

    moving border

--- Shooting_Game/Assets/Scripts/Player.cs
+++ Shooting_Game/Assets/Scripts/Player.cs
@@ -6,14 +6,67 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if ((isTouchRight && h == 1) ||(isTouchLeft &&h== -1)) h = 0;        
+
+        float v = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1)) v = 0;
 
         Vector3 curPos=transform.position;
         Vector3 nextPos=new Vector3(h, v, 0)*speed*Time.deltaTime;
 
         transform.position = curPos + nextPos;
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
+    }
+
 }
