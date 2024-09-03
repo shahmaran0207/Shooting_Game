9f8edef927b8c63f3ccbb8bee2661065b0ab5c40
shahmaran0207 <shahmaran0207@naver.com>
2024-09-03 10:12:53 +0900

    backlground_scrolling

---
+++ Shooting_Game/Assets/Scripts/Background.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed;
+
+    public int startIndex;
+    public int endIndex;
+
+    public Transform[] sprites;
+
+    float viewHeight;
+
+    private void Awake()
+    {
+        viewHeight = Camera.main.orthographicSize*2;
+    }
+    void Update()
+    {
+        Vector3 curPos = transform.position;
+        Vector3 nextPost=Vector3.down*speed*Time.deltaTime;
+        transform.position=curPos+nextPost;
+
+        if (sprites[endIndex].position.y < viewHeight*(-1))
+        {
+            Vector3 backSpritePos = sprites[startIndex].localPosition;
+            Vector3 frontSpritePos = sprites[endIndex].localPosition;
+
+            sprites[endIndex].transform.localPosition = backSpritePos + Vector3.up * viewHeight;
+
+            int startIndexSave = startIndex;
+            startIndex = endIndex;
+            endIndex = startIndexSave - 1 == -1 ? sprites.Length - 1: startIndexSave - 1;
+        }
+    }
+}
