bef2d7e47ea991d941b7885e006448a200caf63e
shahmaran0207 <shahmaran0207@naver.com>
2024-09-04 11:44:46 +0900

    sub weapon - move_1

--- Shooting_Game/Assets/Scripts/Follower.cs
+++ Shooting_Game/Assets/Scripts/Follower.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Follower : MonoBehaviour
@@ -9,11 +10,42 @@ public class Follower : MonoBehaviour
 
     public ObjectManager objectManager;
 
+    public Vector3 followPos;
+
+    public Transform parent;
+
+    public Queue<Vector3> parentPos;
+
+    public int followDelay;
+
     void Update()
     {
+        Watch();
         Follow();
         Fire();
         Reload();
+    }
+
+    void Awake()
+    {
+        //FIFO: First in First out
+        parentPos = new Queue<Vector3>();
+    }
+
+    void Watch()
+    {
+        //Input Pos
+        parentPos.Enqueue(parent.position);
+
+        if (parentPos.Count > followDelay)
+        {
+            //Output Pos
+            followPos = parentPos.Dequeue();
+        }
+    }
+    void Follow()
+    {
+        transform.position = followPos;
     }
 
     void Fire()
@@ -30,11 +62,6 @@ public class Follower : MonoBehaviour
 
 
         curShotDelay = 0;
-    }
-
-    void Follow()
-    {
-       
     }
 
     void Reload()
